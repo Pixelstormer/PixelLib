c3fd7b30deaf921e53edda7f8c127f63cab12aad
Theo Stevens <TS47102@suffolkone.ac.uk>
2019-12-20 18:09:16 +0000

    Modified Assembly info

--- PixelLib/ConsoleHelpers/ColourConsole.cs
+++ PixelLib/ConsoleHelpers/ColourConsole.cs
@@ -6,7 +6,7 @@ namespace PixelLib.ConsoleHelpers
 	/// <summary>
 	/// Allows for far finer control of <see cref="BackgroundColour"/> and <see cref="ForegroundColour"/> settings.
 	/// </summary>
-	public class ColourConsole : CustomConsole
+	public sealed class ColourConsole : CustomConsole
 	{
 		public const char STRINGFORMAT_ESCAPE = '\\';
 		public const char STRINGFORMAT_STARTBLOCK = '{';
