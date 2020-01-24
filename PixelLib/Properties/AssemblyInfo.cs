c279630307aaf8ffe64333b349a71e6d4e62b1c0
Theo Stevens <TS47102@suffolkone.ac.uk>
2020-01-24 12:10:29 +0000

    Updated assembly info again

--- PixelLib/Properties/AssemblyInfo.cs
+++ PixelLib/Properties/AssemblyInfo.cs
@@ -10,12 +10,17 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle ("PixelLib")]
 [assembly: AssemblyDescription ("A miscellaneous C# library created for personal use.")]
-[assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Pixelstorm")]
 [assembly: AssemblyProduct ("PixelLib")]
 [assembly: AssemblyCopyright ("Copyright © Pixelstorm 2020")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration ("Debug")]
+#else
+[assembly: AssemblyConfiguration ("Release")]
+#endif
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -38,4 +43,3 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion ("1.0.0.0")]
 [assembly: AssemblyFileVersion ("1.0.0.0")]
 [assembly: NeutralResourcesLanguage ("en-GB")]
-
